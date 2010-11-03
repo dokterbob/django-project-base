140280b248c0793eb061f4143f213920401dcee1
Mathijs de Bruin <drbob@dokterbob.net>
2010-11-03 21:11:56 +0100

    Small buggy.

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 if settings.DEBUG:
     from staticfiles.urls import staticfiles_urlpatterns
     
-    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns = staticfiles_urlpatterns()
     
     from os import path
     urlpatterns += patterns('django.views', (r'^%s(?P<path>.*)$' % settings.MEDIA_URL,
