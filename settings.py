bcdc453683caa7f6989bb2a68b57232c72bb82ae
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-01-27 15:43:30 +0100

    Log a warning when local settings overrides do not load well.

--- settings.py
+++ settings.py
@@ -1,7 +1,12 @@
 # Smarter settings working, based on work by Rob Golding
 # http://www.robgolding.com/blog/2010/05/03/extending-settings-variables-with-local_settings-py-in-django/
 
+import logging
+logger = logging.getLogger('django-project-base')
+
 try:
     from settings_local import *
 except ImportError:
+    logger.exception('Error importing settings_local')
+    
     from settings_default import *
