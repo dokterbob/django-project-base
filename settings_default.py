0b82cf034c46544510e0d498785d29943a870a34
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-06-17 13:55:36 +0200

    Add admindocs to INSTALLED_APPS if we have it in URLs as well.

--- settings_default.py
+++ settings_default.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.staticfiles',
     'django_extensions',
     'debug_toolbar',
