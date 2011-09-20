441dbdd0dc556c81e7f9650a0c545923a03fe28b
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-09-20 17:41:59 +0200

    Whitespace removal and removed redundant import

--- urls.py
+++ urls.py
@@ -6,14 +6,13 @@ admin.autodiscover()
 
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    
+
     urlpatterns = staticfiles_urlpatterns()
-    
-    from os import path
+
     urlpatterns += patterns('django.views', (r'^%s(?P<path>.*)$' % settings.MEDIA_URL,
-                                              'static.serve', 
+                                              'static.serve',
                                              {'document_root': settings.MEDIA_ROOT }))
-    
+
 else:
     urlpatterns = patterns('')
 
