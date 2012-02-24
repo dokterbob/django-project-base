a6b505cd0de0fd45c7a954da8df0c2cddf900e29
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2012-02-24 15:50:18 +0100

    Serve static files before normal URL parsing.

--- urls.py
+++ urls.py
@@ -8,18 +8,18 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 
-urlpatterns = patterns('',
+# Serve django-staticfiles (only works in DEBUG)
+urlpatterns = staticfiles_urlpatterns()
+
+# Serve media files (only works in DEBUG)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += patterns('',
     #(r'^/', include('foo.urls')),
 
     # Django Admin
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
-
-
-# Serve django-staticfiles (only works in DEBUG)
-urlpatterns += staticfiles_urlpatterns()
-
-# Serve media files (only works in DEBUG)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
